6391a489e4f3196b8531471f93cac142bfcf17c9
guolixiang <guolixiang@ebaolife.com>
2018-01-12 10:11:10 +0800

    update

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
 # -*- coding:utf-8 -*-
 
-app_id = "10661627"
-app_key = "h5xcL0m4TB8fiiFWoysn7uxt"
-app_secret = "HGA1cgXzz80douKQUpII7K77TYWSGcfW"
+image_directory = "D:/screenshots/"
 
+driver_location = "D:\SoftwareInstall\chromedriver_win32\chromedriver.exe"
 
-# 截取题目
-left = 90 # 距离左边的像素
-top = 250 # 距离顶部的像素
-right = 70 # 距离右边的像素
-bottom = 600 #距离顶部的像素
+appcode = '9fdc5f6373ad4c0b8829d8d1a3b23b73'
--- helper.py
+++ helper.py
@@ -3,9 +3,6 @@ from PIL import Image
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-image_directory = "D:/screenshots/"
-driver_location = r"D:\SoftwareInstall\chromedriver_win32\chromedriver.exe"
-
 driver = webdriver.Chrome(config.driver_location)
 # 访问百度
 driver.get('http://www.baidu.com')
@@ -22,21 +19,40 @@ while True:
 
   if not os.path.exists(config.image_directory):
     os.mkdir(config.image_directory)
-  #os.system("adb shell /system/bin/screencap -p /sdcard/screenshot.png")
-  #os.system("adb pull /sdcard/screenshot.png " + imagepath)
+    os.system("adb shell /system/bin/screencap -p /sdcard/screenshot.png")
+    os.system("adb pull /sdcard/screenshot.png " + imagepath)
 
   im = Image.open(imagepath)
   img_size = im.size
   w = im.size[0]
   h = im.size[1]
 
-  region = im.crop((config.left, config.top, w - config.right, config.bottom))  # 裁剪的区域
+  region = im.crop((90, 250, w - 70, 600))  # 裁剪的区域
   region.save(region_path)
 
-  image_data = open(region_path, 'rb').read();
+  f = open(region_path, 'rb')
+  ls_f = base64.b64encode(f.read())
+  f.close()
+  image_byte = bytes.decode(ls_f)
 
-  keyword = baiduocr.get_text_from_image(image_data, config.app_id, config.app_key, config.app_secret)
 
+  url = 'http://text.aliapi.hanvon.com/rt/ws/v1/ocr/text/recg?code=74e51a88-41ec-413e-b162-bd031fe0407e'
+
+  data = {"uid": "118.12.0.12", "lang": "chns", "color": "color",
+          'image': image_byte}
+
+  headers = {"Content-Type": "application/json; charset=UTF-8",
+             "Accept-Content-Type": "application/octet-stream",
+             "Authorization": "APPCODE " + config.appcode
+             }
+  res = requests.post(url, data=json.dumps(data), headers=headers)
+  content = res.text
+  if res.status_code != 200:
+    print("orc识别错误")
+    break
+
+  json_data = json.loads(content)['textResult']
+  keyword = str(json_data).strip().rstrip().lstrip().replace("\r", "").replace("\n","").replace(" ","")
   print("OCR识别内容: " + keyword)
 
   driver.find_element_by_id('kw').send_keys(Keys.CONTROL, 'a')
